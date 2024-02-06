0f1d7f21c115866ec5eba37ac45fd328d3c1327c
Joseviniciusrs <jvschweitzer@gmail.com>
2024-02-06 15:17:34 -0300

    Jump animation and stretch

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
-
+    [Header("Speed setup")]
     public Rigidbody2D myRigidBody;
-
     public Vector2 friction = new Vector2(.1f, 0);
-
     public float speed;
     public float SpeedRun;
-
     public float forceJump = 5;
+
+
+    [Header("Animation setup")]
+    public float JumpScaleY = 1.5f;
+    public float JumpScaleX = 0.8f;
+    public float duration = .3f;
 
     private float _currentSpeed;
 
@@ -58,7 +62,18 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         myRigidBody.velocity = Vector2.up * forceJump;
+        myRigidBody.transform.localScale = Vector2.one;
 
+        DOTween.Kill(myRigidBody.transform);
+
+        HandleScaleJump();
+
+    }
+
+    private void HandleScaleJump()
+    {
+        myRigidBody.transform.DOScaleY(JumpScaleY, duration).SetLoops(2, LoopType.Yoyo);
+        myRigidBody.transform.DOScaleY(JumpScaleX, duration).SetLoops(2, LoopType.Yoyo);
     }
 }
 
