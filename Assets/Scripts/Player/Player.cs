54439b1f9e7faeb760dfb1d94e3be096f2c8e695
Joseviniciusrs <jvschweitzer@gmail.com>
2024-02-06 14:44:34 -0300

    Add jump.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,11 +7,20 @@ public class Player : MonoBehaviour
 
     public Rigidbody2D myRigidBody;
 
-    public Vector2 velocity;
+    public Vector2 friction = new Vector2(.1f, 0);
 
     public float speed;
 
+    public float forceJump = 5;
+
     private void Update()
+    {
+        HandleJump();
+        HandleMovement();
+
+    }
+
+    private void HandleMovement()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
@@ -22,9 +31,25 @@ public class Player : MonoBehaviour
         {
             //myRigidBody.MovePosition(myRigidBody.position + velocity * Time.deltaTime);
             myRigidBody.velocity = new Vector2(speed, myRigidBody.velocity.y);
+        }
 
+
+        if (myRigidBody.velocity.x > 0)
+        {
+            myRigidBody.velocity += friction;
+        }
+        else if (myRigidBody.velocity.x < 0)
+        {
+            myRigidBody.velocity -= friction;
 
         }
+
+    }
+
+    private void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        myRigidBody.velocity = Vector2.up * forceJump;
 
     }
 }
